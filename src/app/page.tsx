cd7ed409d7b36092acef3f94964bc472b5da6fbe
t2023-m0023 <t2023-m0023@t2023-m0023ui-MacBookAir.local>
2025-03-15 16:18:07 +0900

    3-4 진행 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ProductList from "./components/ProductList";
+import NewProductList from "./components/NewProductList";
 export default async function Home() {
 
   return (
     <div>
       <h1>Sparta shop</h1>
+      <NewProductList/>
       <ProductList/>
     </div>
   );
